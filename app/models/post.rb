d16746997aa32bbadafec92d18ee5ec9851eb549
Grant Cloyd <dgcloyd@gmail.com>
2021-07-20 10:38:57 -0500

    commit tree 743580c3e92e880defd2b7823bba9d730060d7a4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
+require 'active_support'
+
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :is_clickbaity
+
+    def is_clickbaity
+       click_bait_words = ["Won't Believe", "Secret", "Top Number", "Guess"]
+       if title != nil && click_bait_words.any? {|word| title.match(word)} == false
+          errors.add(:title, "Not a clickbaity title")
+       end
+    end
 end
